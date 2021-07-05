0b3f906e133e417750fa03d0eaf6682e3dd578b9
Sushrut <sushrutkadam.123@gmail.com>
2021-07-06 00:22:34 +0530

    Repo created

---
+++ script.js
@@ -0,0 +1,25 @@
+function getRow(arg){
+    var tableRow = document.getElementsByTagName("tr");
+    // console.log(tableRow);
+
+    // arg=1;
+    // console.log(tableRow[arg].cells);
+
+    const resultHolder = document.getElementById("result"); 
+    resultHolder.innerHTML = "";                    //to clear the older content
+    
+    const resultTable = document.createElement("table");
+    resultHolder.appendChild(resultTable);
+
+    const resultRow = document.createElement("tr");    
+    resultTable.appendChild(resultRow);
+    
+    var rowElements = tableRow[arg].cells;
+
+    for(var i=0; i<rowElements.length; i++){
+        // console.log(rowElements[i].innerHTML);
+        const resultCol = document.createElement("td");
+        resultCol.innerHTML = rowElements[i].innerHTML;
+        resultRow.appendChild(resultCol);
+    }
+}
